28157d965905a997fa95dcc3c4c4623dc3fae0d4
giz73 <nneven@proton.me>
2025-06-27 20:38:33 +0200

    dodana oba ugovora u contract.js

--- frontend/crypto-lotto/src/utils/contract.js
+++ frontend/crypto-lotto/src/utils/contract.js
@@ -1,9 +1,22 @@
 // utils/contract.js
 import { ethers } from "ethers";
-import { CONTRACT_ADDRESS, CONTRACT_ABI } from "../contracts/CryptoLotto";
+import {
+  CONTRACT_ADDRESS as LOTTO_ADDRESS,
+  CONTRACT_ABI as LOTTO_ABI,
+} from "../contracts/CryptoLotto";
+import {
+  CONTRACT_ADDRESS as NFT_ADDRESS,
+  CONTRACT_ABI as NFT_ABI,
+} from "../contracts/LottoNFT";
 
 export function getLottoContract() {
   const provider = new ethers.providers.Web3Provider(window.ethereum);
   const signer = provider.getSigner();
-  return new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, signer);
+  return new ethers.Contract(LOTTO_ADDRESS, LOTTO_ABI, signer);
+}
+
+export function getNFTContract() {
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+  return new ethers.Contract(NFT_ADDRESS, NFT_ABI, signer);
 }
