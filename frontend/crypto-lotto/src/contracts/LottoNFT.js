ec46a38e82d58968d194f824f9cf566b2c1f87bc
giz73 <nneven@proton.me>
2025-06-27 20:35:07 +0200

    dodani adresa i  ABI za LottoNFT

---
+++ frontend/crypto-lotto/src/contracts/LottoNFT.js
@@ -0,0 +1,3 @@
+import abi from "./LottoNFT.json"; // abi za LottoNFT
+export const CONTRACT_ADDRESS = "0x08688c7753eA59391A46aB3209139B995a9B3c67"; // Adresa LottoNFT ugovora
+export const CONTRACT_ABI = abi;
