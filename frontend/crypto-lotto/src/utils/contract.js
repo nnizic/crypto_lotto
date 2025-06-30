7f1e0052b3f40b9740b4d9af6a515090c7418c0c
giz73 <nneven@proton.me>
2025-06-30 20:25:34 +0200

    novi isAdmin

--- frontend/crypto-lotto/src/utils/contract.js
+++ frontend/crypto-lotto/src/utils/contract.js
@@ -22,8 +22,19 @@ export function getNFTContract() {
 }
 
 // Provjera igrač ili admin
+
 export async function isAdmin(address) {
+  if (!address) return false; //ako adresa nije definirana, korisnik nije admin!!
+
   const contract = getLottoContract();
-  const owner = await contract.owner();
-  return address.toLowerCase() === owner.toLowerCase();
+  try {
+    const owner = await contract.owner();
+    return address.toLowerCase() === owner.toLowerCase();
+  } catch (error) {
+    console.error("Greška prilikom dohvaćanja ownera:", error);
+    return false;
+  }
 }
+
+
+
