76b8346a96ec874d4a69abe926ca850cb5101f08
giz73 <nneven@proton.me>
2025-07-01 18:47:19 +0200

    dodana funkcija za dohvat nft-ova

--- frontend/crypto-lotto/src/contracts/LottoNFT.js
+++ frontend/crypto-lotto/src/contracts/LottoNFT.js
@@ -1,3 +1,12 @@
-import abi from "./LottoNFT.json"; // abi za LottoNFT
-export const CONTRACT_ADDRESS = "0x08688c7753eA59391A46aB3209139B995a9B3c67"; // Adresa LottoNFT ugovora
+import { ethers } from "ethers";
+import abi from "./LottoNFT.json";
+
+export const CONTRACT_ADDRESS = "0x08688c7753eA59391A46aB3209139B995a9B3c67";
 export const CONTRACT_ABI = abi;
+
+export async function getNFTsByOwner(provider, ownerAddress) {
+  const contract = new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, provider);
+  const tokenIds = await contract.getNFTsByOwner(ownerAddress);
+  return tokenIds.map(id => id.toNumber()); // pretvaranje BigNumber objekata u broj
+}
+
